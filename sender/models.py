53e53cee09f6bfee0c1e6c807c683103c2bd7ec3
Alexander Averin <alexaverin201156@gmail.com>
2025-05-30 15:15:49 +0300

    message CRUD up

--- sender/models.py
+++ sender/models.py
@@ -1,23 +1,23 @@
-from logging import Manager
-
 from django.db import models
 from django.db.models.functions import Now
 from django_currentuser.db.models import CurrentUserField
 
 class Client(models.Model):
+    """
+    модель получателя рассылки (клиента)
+    """
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=150, verbose_name="Ф.И.О.")
     comment = models.TextField(max_length=1000, verbose_name="Комментарий", blank=True)
 
 
-"""
-1. Управление клиентами
-Реализовать возможность добавлять, просматривать, редактировать и удалять получателей рассылки (клиентов).
 
-Модель «Получатель рассылки»:
 
-Email (строка, уникальное).
-Ф. И. О. (строка).
-Комментарий (текст).
-"""
-# Create your models here.
+class Message(models.Model):
+    """
+    модель сообщения в рассылке
+    """
+    topic = models.CharField(max_length=150, verbose_name="Тема")
+    text = models.TextField(max_length=5000, verbose_name="Сообщение")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
--- sender/urls.py
+++ sender/urls.py
@@ -7,17 +7,8 @@ urlpatterns = [
     path("client_details/<int:pk>/", views.ClientDetailView.as_view(), name="client_details"),
     path("edit_client/<int:pk>/", views.ClientUpdateView.as_view(), name="edit_client"),
     path("delete_client/<int:pk>/", views.ClientDeleteView.as_view(), name="delete_client"),
+    path("new_message/", views.MessageCreateView.as_view(), name="new_message"),
+    path("message_details/<int:pk>/", views.MessageDetailView.as_view(), name="message_details"),
+    path("edit_message/<int:pk>/", views.MessageUpdateView.as_view(), name="edit_message"),
+    path("delete_message/<int:pk>/", views.MessageDeleteView.as_view(), name="delete_message"),
 ]
-"""
-path("", views.ClientListView.as_view(), name="home"),
-path("details/<int:pk>/", views.ClientDetailView.as_view(), name="client_details"),
-path("new_client/", views.ClientCreateView.as_view(), name="new_client"),
-path("edit/<int:pk>/", views.ClientUpdateView.as_view(), name="edit_client"),
-path("new_category/", views.CategoryCreateView.as_view(), name="new_category"),
-path("contacts/", views.ContactsView.as_view(), name="contacts"),
-path("feedback/", views.FeedbackFormView.as_view(), name="feedback"),
-path("posted_info/<int:pk>/", views.PostedMessageView.as_view(), name="posted_info"),
-path("delete/<int:pk>/", views.ClientDeleteView.as_view(), name="delete_client"),
-path("send_letter/", views.send_letter, name="send_letter"),
-path("category_clients/<int:pk>/", views.CategoryClientsListView.as_view(), name="category_clients"),
-"""
