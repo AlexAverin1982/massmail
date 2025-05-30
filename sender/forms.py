9fc35844d9269c415e732608fe794b5ab2359599
Alexander Averin <alexaverin201156@gmail.com>
2025-05-30 15:15:49 +0300

    message CRUD up

--- sender/forms.py
+++ sender/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Client
+from .models import Client, Message
 from .mixins import FormControlMixin
 
 class ClientCreateForm(FormControlMixin, forms.ModelForm):
     class Meta:
         model = Client
         fields = ['full_name', 'email', 'comment']
+
+class MessageCreateForm(FormControlMixin, forms.ModelForm):
+    class Meta:
+        model = Message
+        fields = ['topic', 'text']
+
--- sender/views.py
+++ sender/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views import generic
 from django.urls import reverse_lazy, reverse
 from django.core.cache import cache
@@ -7,25 +7,20 @@ from django.utils.decorators import method_decorator
 
 from typing_extensions import Any
 
+from .models import Client, Message
+from .forms import ClientCreateForm, MessageCreateForm
 
-from .models import Client
-from .forms import ClientCreateForm
 
-class HomeView(generic.ListView):
-    model = Client
+class HomeView(generic.TemplateView):
     template_name = "home.html"
-    context_object_name = 'clients'
-    paginate_by = 10
 
-    def get_queryset(self):
-        
-        key_name = f"clients_queryset"
-        queryset = cache.get(key_name)
-        if not queryset:
-            queryset = Client.objects.all()
-            cache.set(key_name, queryset, 60 * 15)
-
-        return queryset
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'clients': Client.objects.all().order_by('full_name'),
+            'messages': Message.objects.all().order_by('-updated_at'),
+        })
+        return context
 
 
 class ClientCreateView(generic.CreateView):
@@ -51,7 +46,8 @@ class ClientDetailView(generic.DetailView):
     template_name = "client_details.html"
     context_object_name = 'client'
 
-# LoginRequiredMixin, 
+
+# LoginRequiredMixin,
 class ClientUpdateView(generic.UpdateView):
     model = Client
     form_class = ClientCreateForm
@@ -78,4 +74,50 @@ class ClientDeleteView(generic.DeleteView):
         return redirect('home')
 
 
+#######################################################################################################################
 
+
+class MessageCreateView(generic.CreateView):
+    model = Message
+    form_class = MessageCreateForm
+    template_name = 'new_message.html'
+    context_object_name = 'message'
+
+    extra_context = {
+        'title': 'Новое сообщение',
+    }
+    success_url = reverse_lazy('home')
+
+
+class MessageDetailView(generic.DetailView):
+    model = Message
+    template_name = "message_details.html"
+    context_object_name = 'message'
+
+
+class MessageUpdateView(generic.UpdateView):
+    model = Message
+    form_class = MessageCreateForm
+    template_name = 'new_message.html'
+    # success_url = reverse_lazy('home')
+    extra_context = {
+        'title': 'Редактирование сообщения',
+        'message_editing_mode': True,
+    }
+
+    def get_success_url(self):
+        return reverse("message_details", kwargs=self.kwargs)
+
+
+class MessageDeleteView(generic.DeleteView):
+    model = Message
+    success_url = reverse_lazy("home")
+    context_object_name = 'message'
+    template_name = 'delete_message.html'
+
+    def post(self, request, *args, **kwargs) -> Any:
+        obj = Message.objects.get(id=kwargs['pk'])
+        super(MessageDeleteView, self).post(request, *args, **kwargs)
+        return redirect('home')
+
+#######################################################################################################################
