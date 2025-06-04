61045dc9522f1cca92ba6000827e1c95cf858748
Alexander Averin <alexaverin201156@gmail.com>
2025-06-04 13:10:36 +0300

    had to delete users app

--- config/settings.py
+++ config/settings.py
@@ -109,8 +109,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
-# AUTH_USER_MODEL = 'users.CustomUser'
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
